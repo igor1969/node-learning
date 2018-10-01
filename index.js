ffc6c252206663421adfbe560f94f7dcf9ff82e3
igor-local <igor.kopanev@gmail.com>
2018-10-01 14:49:12 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const clc = require("cli-color");
+const beep = require('beepbeep')
+const term = require( 'terminal-kit' ).terminal ;
+
+beep()
+console.log(clc.red('Wait 2 sec please...'))
+
+term.slowTyping(
+	'Loading........\n' ,
+	{ flashStyle: term.brightWhite } ,
+	function() { process.exit() ; }
+) ;
+
+
+function helloFromYellow() {
+    console.log(clc.yellow('Hello! I am yellow notifyer!!!'))
+    beep()
+}
+setTimeout(helloFromYellow, 2000)
+
+
+
+
+
+
